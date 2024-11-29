21450337806dbf1f58d100e87e7382a07292a864
Moana <jeoffreyb88@gmail.com>
2024-11-30 00:13:35 +0900

    Showing telegram user name as test

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,36 @@ import Leaderboard from '@/pages/leaderboard';
 import Quests from '@/pages/quests';
 import MyTribe from '@/pages/my-tribe';
 import Layout from '@/components/layout/layout';
+import { useEffect, useState } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
 
+    const [telegramUser, setTelegramUser] = useState(null);
+    useEffect(() => {
+        // Ensure the Telegram Web Apps object exists
+        if (window.Telegram?.WebApp) {
+          const user = window.Telegram.WebApp.initDataUnsafe?.user;
+    
+          if (user) {
+            setTelegramUser(user); // Set the user info in state
+          } else {
+            console.warn("No user data available");
+          }
+        }
+      }, []);
+
     return (
         <QueryClientProvider client={queryClient}>
             <TimerProvider initialSeconds={14}>
+            {telegramUser ? (
+        <p>
+          Welcome, {telegramUser.username || telegramUser.first_name}!
+        </p>
+      ) : (
+        <p>Loading user info...</p>
+      )}
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Layout><Home /></Layout>} />

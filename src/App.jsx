c101348aa1079cc0455a00ba1bb02df52c37d667
Moana <jeoffreyb88@gmail.com>
2024-12-01 04:16:55 +0900

    Testing with telegram_user

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
             const user = window.Telegram.WebApp.initDataUnsafe?.user;
             if (user) {
                 setTelegramUser(user);
-                console.log(window.Telegram.WebApp.initDataUnsafe, "_++_+__+")
+                console.log(user?.username, "_++_+__+", user?.id)
             } else {
                 window.alert("No user data available");
             }
---
+++ src/api/endpoints.js
@@ -0,0 +1,7 @@
+const API_ENDPOINTS = {
+  GET_USERS: "/user",
+  GET_USER_INFO: "/user/info",
+  CREATE_USER: "/user",
+};
+
+export default API_ENDPOINTS;

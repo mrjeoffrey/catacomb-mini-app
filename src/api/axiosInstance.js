de040b03219f729c11ccb457950b63d775d767bf
Moana <jeoffreyb88@gmail.com>
2024-12-04 15:46:39 +0900

    Fixing backend url issue

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://bot.api.catacomb.fyi/api", // Backend URL
+  baseURL: "https://bot-api.catacomb.fyi/api", // Backend URL
   headers: {
     "Content-Type": "application/json",
   },

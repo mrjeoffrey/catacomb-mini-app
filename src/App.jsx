20264899eaacb4dfc4fdd239dc16453968a515ef
Moana <jeoffreyb88@gmail.com>
2024-12-05 12:21:08 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies.
  */
-import { Routes, Route, BrowserRouter, useSearchParams } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { TimerProvider } from '@/contexts/timer-context';
 
 
@@ -21,79 +21,76 @@ import { LoadingPanel } from '@/components/loading/loading';
 
 
 function App() {
-    // const [loading, setLoading] = useState(true)
-    // useEffect(() => {
-    //     const handleUserCheck = async () => {
-    //         if (window.Telegram?.WebApp) {
-    //             const user = window.Telegram.WebApp.initDataUnsafe?.user;
-    //             if (user) {
-    //                 try {
-    //                     // Check if user exists in the database
-    //                     const response = await axiosInstance.post('/user/info', {
-    //                         telegram_id: user.id//6430530130,
-    //                     });
-    //                     console.log(response, "response")
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const handleUserCheck = async () => {
+            if (window.Telegram?.WebApp) {
+                const user = window.Telegram.WebApp.initDataUnsafe?.user;
+                if (user) {
+                    try {
+                        // Check if user exists in the database
+                        const response = await axiosInstance.post('/user/info', {
+                            telegram_id: user.id//6430530130,
+                        });
+                        console.log(response, "response")
                         
-    //                     // If user exists, log their info
-    //                     if (response.data) {
-    //                         // setLoading(false)
-    //                         console.log('Existing user:', response.data);
-    //                     }
-    //                 } catch (error) {
-    //                     console.log(error, "error.response")
-    //                     // If user doesn't exist, create a new user
-    //                     if (error.response?.status === 404) {
-    //                         try {
-    //                             const newUserResponse = await axiosInstance.post('/user', {
-    //                                 telegram_id: user.id,
-    //                                 username: user.username || user.first_name,
-    //                                 wallet_address: null, // Add logic for wallet address if available
-    //                                 IP_address: window.location.hostname, // Example IP address logic
-    //                             });
+                        // If user exists, log their info
+                        if (response.data) {
+                            setLoading(false)
+                            console.log('Existing user:', response.data);
+                        }
+                    } catch (error) {
+                        console.log(error, "error.response")
+                        // If user doesn't exist, create a new user
+                        if (error.response?.status === 404) {
+                            try {
+                                const newUserResponse = await axiosInstance.post('/user', {
+                                    telegram_id: user.id,
+                                    username: user.username || user.first_name,
+                                    wallet_address: null, // Add logic for wallet address if available
+                                    IP_address: window.location.hostname, // Example IP address logic
+                                });
 
-    //                             console.log('New user created:', newUserResponse.data.user);
-    //                             // setLoading(false)
+                                console.log('New user created:', newUserResponse.data.user);
+                                setLoading(false)
 
-    //                         } catch (creationError) {
-    //                             console.error('Error creating new user:', creationError);
-    //                         }
-    //                     } else {
-    //                         console.error('Error fetching user info:', error);
-    //                     }
-    //                 }
-    //             } else {
-    //                 window.alert('No user data available');
-    //             }
-    //             ;
-    //         }
-    //     };
-    //     handleUserCheck();
-    // }, []);
+                            } catch (creationError) {
+                                console.error('Error creating new user:', creationError);
+                            }
+                        } else {
+                            console.error('Error fetching user info:', error);
+                        }
+                    }
+                } else {
+                    window.alert('No user data available');
+                }
+                ;
+            }
+        };
+        handleUserCheck();
+    }, []);
 
-//     const [searchParams] = useSearchParams();
-//   const referredBy = searchParams.get('referred_by')
     console.log(window.Telegram.WebApp.initDataUnsafe.start_param, "start_param");
-    // const { data: userInfo, refetch } = useUserInfo(window.Telegram.WebApp.initDataUnsafe?.user?.id);
+    const { data: userInfo, refetch } = useUserInfo(window.Telegram.WebApp.initDataUnsafe?.user?.id);
 
     return (
-            // userInfo?
-            // <TimerProvider initialSeconds={userInfo?.seconds} time_remaining={userInfo?.remainingSeconds}>
+            userInfo?
+            <TimerProvider initialSeconds={userInfo?.seconds} time_remaining={userInfo?.remainingSeconds}>
                 <BrowserRouter>
-                    {/* {loading?<LoadingPanel/>: */}
-                    {/* // <Layout userInfo={userInfo}> */}
-                       Reffered By {window.Telegram.WebApp?.initDataUnsafe?.start_param}
+                    {loading?<LoadingPanel/>:
+                    <Layout userInfo={userInfo}>
+                       Reffered {window.Telegram.WebApp?.initDataUnsafe?.start_param}
                     <Routes>
-                        <Route path="/" element={<Home refetch={()=>{}}/>} />
-                        <Route path="/referred_by/:referred_by" element={<Home refetch={()=>{}}/>} />
+                        <Route path="/" element={<Home refetch={refetch}/>} />
+                        <Route path="/referred_by/:referred_by" element={<Home refetch={refetch}/>} />
                         <Route path="/leaderboard" element={<Leaderboard />} />
                         <Route path="/quests" element={<Quests />} />
                         <Route path="/my-tribe" element={<MyTribe />} />
                     </Routes>
-                    {/* // </Layout> */}
-{/* } */}
+                    </Layout>
+}
                 </BrowserRouter>
-            // </TimerProvider>
-            // :<LoadingPanel/>
+            </TimerProvider>:<LoadingPanel/>
     );
 }
 

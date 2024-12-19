49d3035e4f4d55f13dbe05064010acb4734ab60c
Moana <jeoffreyb88@gmail.com>
2024-12-18 19:29:51 -0800

    Adding Splash Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-/**
- * External dependencies.
- */
 import { useEffect, useState } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
@@ -17,6 +14,7 @@ import axiosInstance from './api/axiosInstance';
 import { useUserInfo } from './queries/useUserInfo';
 import { LoadingPanel } from '@/components/loading/loading';
 import { useGetTasks } from './queries/useTasks';
+import SplashPage from './pages/splash';
 
 const getLocationForIP = async (ip) => {
   if (!ip) throw new Error('IP address is required');
@@ -29,44 +27,36 @@ const getLocationForIP = async (ip) => {
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [showSplash, setShowSplash] = useState(true); // State to manage splash visibility
 
   useEffect(() => {
     const fetchIPAddress = async () => {
       try {
         const response = await fetch(import.meta.env.VITE_CHECK_IP_URL);
-
         const ip = await response.text();
-
         return ip.trim();
       } catch (error) {
-        console.log(error)
+        console.log(error);
         return null;
       }
     };
 
     const handleUserCheck = async () => {
-
       if (window.Telegram?.WebApp) {
-
         const user = window.Telegram.WebApp.initDataUnsafe?.user;
 
         if (user) {
           try {
-
             const response = await axiosInstance.post('/user/info', {
-              telegram_id:user.id,
+              telegram_id: user.id,
             });
             if (response.data) {
               setLoading(false);
-
             }
           } catch (error) {
-
             if (error.response?.status === 404) {
-
               try {
                 const ipAddress = await fetchIPAddress();
-
                 const locationData = await getLocationForIP(ipAddress);
 
                 const locationString = locationData?.country
@@ -84,7 +74,7 @@ function App() {
 
                 setLoading(false);
               } catch (creationError) {
-                console.log(creationError)
+                console.log(creationError);
               }
             }
           }
@@ -92,16 +82,21 @@ function App() {
       }
     };
 
-
     handleUserCheck();
   }, []);
-  const { data: userInfo, refetch } = useUserInfo(window.Telegram.WebApp.initDataUnsafe?.user?.id
+
+  const { data: userInfo, refetch } = useUserInfo(
+    window.Telegram.WebApp.initDataUnsafe?.user?.id
   );
   const { data: tasks, isLoading } = useGetTasks();
 
-  useEffect(() => {
+  const handleStartApp = () => {
+    setShowSplash(false);
+  };
 
-  }, [userInfo]);
+  if (showSplash) {
+    return <SplashPage onStart={handleStartApp} />;
+  }
 
   return userInfo ? (
     <TimerProvider
@@ -111,17 +106,22 @@ function App() {
     >
       <BrowserRouter>
         {loading ? (
-          <>
-            <LoadingPanel />
-          </>
+          <LoadingPanel />
         ) : (
           <Layout userInfo={userInfo} tasks={tasks}>
             <Routes>
               <Route path="/" element={<Home refetch={refetch} />} />
-              <Route path="/leaderboard" element={<Leaderboard userInfo={userInfo}/>} />
+              <Route path="/leaderboard" element={<Leaderboard userInfo={userInfo} />} />
               <Route
                 path="/quests"
-                element={<Quests userInfo={userInfo} refetch={refetch} tasks={tasks} isLoading={isLoading} />}
+                element={
+                  <Quests
+                    userInfo={userInfo}
+                    refetch={refetch}
+                    tasks={tasks}
+                    isLoading={isLoading}
+                  />
+                }
               />
               <Route path="/my-tribe" element={<MyTribe userInfo={userInfo} />} />
             </Routes>
@@ -130,10 +130,7 @@ function App() {
       </BrowserRouter>
     </TimerProvider>
   ) : (
-    <>
-      
-      <LoadingPanel />
-    </>
+    <LoadingPanel />
   );
 }
 
---
+++ src/pages/splash.jsx
@@ -0,0 +1,8 @@
+const SplashPage = ({ onStart }) => (
+    <div className="splash-page">
+      <h1>Welcome to the App!</h1>
+      <button onClick={onStart}>Start</button>
+    </div>
+);
+
+export default SplashPage
